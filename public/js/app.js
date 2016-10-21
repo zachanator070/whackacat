008a1653a8dd17eab3cf40a6bd42a0cb2dc0841e
zjohnson <zjohnson@adobe.com>
2016-10-21 16:31:20 -0600

    init scores working

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 
-var app = angular.module('app', []);
+var app = angular.module('whackacat', []);
 
-app.controller('whackacat', function($scope){
-
+app.controller('game', function($scope){
+  $.ajax({
+    url: "scores",
+    method: "GET",
+    success: function(data, status){
+      console.log(data);
+      $scope.scores = data;
+      $scope.$apply();
+    }
+  });
 });
--- routes/scores.js
+++ routes/scores.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var scores = [];
+var scores = [{name:"Zach", score:"9000"},{name:"Jeff", score:"2"}];
 
 router.get('/',function(req,res){
-  req.send(scores);
+  res.send(scores);
 });
 
 router.post('/',function(req,res){
